4b34fa7962de63a402469736819a53fd3a00cc4e
ghosiajaved <ghosia.javed15@gmail.com>
2024-08-14 11:10:07 +0500

    Changed react statement

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           <div className='intro'>
             <h1>Hello {name}</h1><br></br>
             <h1>React App With Ghosia <br></br>
-              React is {5 + 5} times better than JSX</h1>
+              React is {10 + 5} times better than JSX</h1>
             <h3>{text}</h3>
             <p>This is my first react app and
               I am happy to work on it</p>
